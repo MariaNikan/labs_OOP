b9d2bfb043e53d181ad84df037795ba06d34c90f
MariaNikan <nikandrova.m@mail.ru>
2017-05-13 10:01:31 +0300

    1.2
    изменение файла flipbyte

--- lab1task2/flipbyte/flipbyte/flipbyte.cpp
+++ lab1task2/flipbyte/flipbyte/flipbyte.cpp
@@ -4,48 +4,62 @@
 
 using namespace std;
 
-void Dec2bin(int number,string bin_number)
+string Dec2bin(int number)
 {
-	for (int i = 0; i < 8; i++)
+	string bin_number = "";
+	for (int i = 7; i >= 0; i--)
 	{
 		if (number % 2 == 1)
 		{
-			bin_number[i] = '1';
+			bin_number = '1' + bin_number;
 		}
 		else
 		{
-			bin_number[i] = '0';
+			bin_number = '0' + bin_number;
 		}
 		number = number / 2;
 	}
+	return bin_number;
 }
 
-void Bin2dec(string bin_number, int number)
+int Bin2dec(string bin_number)
 {
+	int number = 0;
 	int k = 7;
 	for (int i = 0; i < 8; i++)
 	{
 		if (bin_number[i] == '1')
 		{
-			number = +(2 ^ k);
+			int temp = 1;
+			for (int j = 1; j <= k; j++)
+			{
+				temp = temp * 2;
+			}
+			number = number + temp;
 		}
 		k--;
 	}
+	return number;
+}
+
+string FlipNumber(string number)
+{
+	int j = 7;
+	for (int i = 0; i < 4; i++)
+	{
+		char temp = number[i];
+		number[i] = number[j];
+		number[j] = temp;
+		j--;
+	}
+	return number;
 }
 
 void Flip(int number)
 {
-	string twoBitNumber = "";
-	Dec2bin(number, twoBitNumber);
-	int j = 7;
-	for (int i = 0; i < 4; i++)
-	{
-		char temp = twoBitNumber[i];
-		twoBitNumber[i] = twoBitNumber[j];
-		twoBitNumber[j] = temp;
-		j--;
-	}
-	Bin2dec(twoBitNumber, number);
+	string bin_number = Dec2bin(number);
+	string flip_bin_number  = FlipNumber(bin_number);
+	number = Bin2dec(flip_bin_number);
 	cout << number << endl;
 }
 
@@ -57,11 +71,11 @@ int main(int arg_c, char * arg_v[])
 		return 1;
 	}
 	string inputNumber = arg_v[1];
-	int number;
+	int number = 0;
 	int n = size(inputNumber);
-	for (int i = 0; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		int k = int(inputNumber[i]);
+		int k = int(inputNumber[i]) - 48;
 		number = number * 10 + k;
 	}
 	if ((number >= 0)&&(number <= 255))

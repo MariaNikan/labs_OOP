554723c2b5b6a0962e2e6b6338b10931faec775a
MariaNikan <nikandrova.m@mail.ru>
2017-05-13 10:26:01 +0300

    1.2

--- lab1task2/flipbyte/flipbyte/flipbyte.bat
+++ lab1task2/flipbyte/flipbyte/flipbyte.bat
@@ -1,19 +1,11 @@
 set PROGRAM="%~1"
-%PROGRAM% 10 > 80
+%PROGRAM% 6 > number2.txt
 if %ERRORLEVEL% EQU 1 goto error 
-%PROGRAM% 12 > 48
+%PROGRAM% 0 > number1.txt
 if %ERRORLEVEL% EQU 1 goto error
-%PROGRAM% 0 > 255
-if %ERRORLEVEL% NEQ 1 goto error
-%PROGRAM% 0 > 0
+%PROGRAM% 256 > number3.txt
 if %ERRORLEVEL% EQU 1 goto error
-%PROGRAM% 1 > 128
-if %ERRORLEVEL% EQU 1 goto error
-%PROGRAM% 255 > 0
-if %ERRORLEVEL% NEQ 1 goto error
-%PROGRAM% 255 > 255
-if %ERRORLEVEL% EQU 1 goto error
-%PROGRAM% 256 > "Enter correct number"
+%PROGRAM%  > number4.txt
 if %ERRORLEVEL% EQU 1 goto error
 echo Successful program testing
 exit 0

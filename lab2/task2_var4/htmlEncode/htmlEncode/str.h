f3aa8302fe49d74046b8ab6eccc2474185e057de
MariaNikan <nikandrova.m@mail.ru>
2017-09-09 09:21:50 +0300

    change

--- lab2/task2_var4/htmlEncode/htmlEncode/str.h
+++ lab2/task2_var4/htmlEncode/htmlEncode/str.h
@@ -1,7 +1,5 @@
 #pragma once
 
-using namespace std;
-
-string ReplaceSymbol(std::string& line, char symbol, std::string text);
-string HtmlEncode(std::string const& text);
+std::string ReplaceSymbol(std::string& line, char symbol, std::string text);
+std::string HtmlEncode(std::string const& text);
 void PrintOutString(std::ostream& stream, std::string& text);
